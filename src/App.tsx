6f272f4f06cf539665ee78de1a066b01e569bbf3
Toops61 <toops61@gmail.com>
2023-09-01 08:41:05 +0200

    install project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="*"></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
