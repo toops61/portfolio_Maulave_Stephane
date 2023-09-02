327e92abb1d92fa072d105ed3adba1ee5552f73b
Toops61 <toops61@gmail.com>
2023-09-02 19:29:04 +0200

    add title cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
-import { Route, Routes } from "react-router-dom"
+import { useEffect, useState } from "react";
 
 function App() {
 
+  const [titleText, setTitleText] = useState('');
+  const [indexTitle, setIndexTitle] = useState(0);
+  const [textAppeared, setTextAppeared] = useState(false);
+
+  const titleArray = "Portfolio".split("");
+
+  useEffect(() => {
+    const duration = (Math.random() + 0.5) * 400;
+    setTimeout(() => {
+      if (titleText.length < titleArray.length) {
+        setTitleText(titleText + titleArray[indexTitle]);
+        setIndexTitle(indexTitle + 1);
+      } else {
+        setTextAppeared(true);
+      }
+    }, duration);
+  }, [titleText]);
+
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/"></Route>
-        <Route path="*"></Route>
-      </Routes>
-    </div>
+    <main className="App">
+      <div className="title-container">
+        <h1>{titleText}</h1>
+      </div>
+      {textAppeared ? <section>
+        <div className="name-container">
+          <h2>Maulavé Stéphane</h2>
+          <div className="under-line"></div>
+        </div>
+        <div className="about">
+          <h3>A propos</h3>
+          <p>Formation chez Open Classrooms en 2021</p>
+          <p>Développeur Frontend chez <a href="https://www.seiprascore.com/">Seipra Score</a></p>
+        </div>
+      </section> : <></>}
+    </main>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './styles/style.css';
-import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <App />
 )
