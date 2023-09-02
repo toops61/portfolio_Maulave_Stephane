5f05a9a6e1d23041e3308b59d2daf40f3728dd43
Toops61 <toops61@gmail.com>
2023-09-02 21:46:38 +0200

    add about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import ocLogo from './assets/Logo_OpenClassrooms.png';
+import seipraLogo from './assets/seipra.webp';
+import sqlLogo from './assets/MySQL.svg';
+import tsLogo from './assets/Typescript_logo.svg';
 
 function App() {
 
   const [titleText, setTitleText] = useState('');
   const [indexTitle, setIndexTitle] = useState(0);
-  const [textAppeared, setTextAppeared] = useState(false);
 
   const titleArray = "Portfolio".split("");
+
+  const nameRef = useRef<HTMLHeadingElement>(null);
+  const aboutRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const duration = (Math.random() + 0.5) * 400;
@@ -15,7 +21,8 @@ function App() {
         setTitleText(titleText + titleArray[indexTitle]);
         setIndexTitle(indexTitle + 1);
       } else {
-        setTextAppeared(true);
+        nameRef?.current?.classList.add('appears');
+        aboutRef?.current?.classList.add('appears');
       }
     }, duration);
   }, [titleText]);
@@ -23,20 +30,40 @@ function App() {
 
   return (
     <main className="App">
+      <div className="github-link"><a href="https://github.com/toops61" target="_blank" rel="noreferrer"></a></div>
       <div className="title-container">
         <h1>{titleText}</h1>
       </div>
-      {textAppeared ? <section>
-        <div className="name-container">
-          <h2>Maulavé Stéphane</h2>
-          <div className="under-line"></div>
+      <section className="name-container" ref={nameRef}>
+        <h2>Maulavé Stéphane</h2>
+        <div className="under-line"></div>
+      </section>
+      <section className="about" ref={aboutRef}>
+        <h3>A propos</h3>
+        <div className="about-container">
+          <div className="oc-logo">
+            <img src={ocLogo} alt="Open Classrooms" />
+          </div>
+          <p>Formation développeur web chez Open Classrooms en 2021</p>
         </div>
-        <div className="about">
-          <h3>A propos</h3>
-          <p>Formation chez Open Classrooms en 2021</p>
-          <p>Développeur Frontend chez <a href="https://www.seiprascore.com/">Seipra Score</a></p>
+        <div className="about-container">
+          <div className="seipra-logo">
+            <img src={seipraLogo} alt="Seipra Score" />
+          </div>
+          <p>Développeur Frontend chez <a href="https://www.seiprascore.com/" target="_blank" rel="noreferrer">Seipra Score</a> depuis 2021</p>
         </div>
-      </section> : <></>}
+        <div className="about-container">
+          <p>Languages / Libriaries : </p>
+          <div className="logo html"></div>
+          <div className="logo css"></div>
+          <div className="logo js"></div>
+          <div className="logo typescript"><img src={tsLogo} alt="TypeScript" /></div>
+          <div className="logo sql"><img src={sqlLogo} alt="My SQL" /></div>
+        </div>
+        <div className="about-container">
+          <p>Frameworks / Libriaries : </p>
+        </div>
+      </section>
     </main>
   )
 }
